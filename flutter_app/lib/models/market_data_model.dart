b7faf478997cfe13b12b414e2e340bcd620e7ece
Sukhaman Singh <sukhamandev@gmail.com>
2026-01-16 15:10:52 -0500

    updated MarketData model with fromJson and toJson support

--- flutter_app/lib/models/market_data_model.dart
+++ flutter_app/lib/models/market_data_model.dart
@@ -1,22 +1,37 @@
-// TODO: Create MarketData model class
-// Required fields:
-// - symbol (String)
-// - price (double)
-// - change24h (double)
-// - changePercent24h (double)
-// - volume (double)
-//
-// Add a factory constructor fromJson that parses the JSON response
-// Example JSON structure from API:
-// {
-//   "symbol": "BTC/USD",
-//   "price": 43250.50,
-//   "change24h": 2.5,
-//   "changePercent24h": 2.5,
-//   "volume": 1250000000
-// }
-
 class MarketData {
-  // TODO: Add required fields and constructor
-  // TODO: Add factory MarketData.fromJson(Map<String, dynamic> json)
+  final String symbol;
+  final double price;
+  final double change24h;
+  final double changePercent24h;
+  final double volume;
+
+  MarketData({
+    required this.symbol,
+    required this.price,
+    required this.change24h,
+    required this.changePercent24h,
+    required this.volume,
+  });
+
+  /// Factory constructor to create a MarketData object from JSON
+  factory MarketData.fromJson(Map<String, dynamic> json) {
+    return MarketData(
+      symbol: json['symbol'] as String? ?? '',
+      price: (json['price'] as num?)?.toDouble() ?? 0.0,
+      change24h: (json['change24h'] as num?)?.toDouble() ?? 0.0,
+      changePercent24h: (json['changePercent24h'] as num?)?.toDouble() ?? 0.0,
+      volume: (json['volume'] as num?)?.toDouble() ?? 0.0,
+    );
+  }
+
+  /// Optional: convert MarketData back to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'symbol': symbol,
+      'price': price,
+      'change24h': change24h,
+      'changePercent24h': changePercent24h,
+      'volume': volume,
+    };
+  }
 }
